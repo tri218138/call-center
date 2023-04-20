0a5b3e4eb27e11524389360ba5963c54028372a0
ducthuy-ng <46134513+ducthuy-ng@users.noreply.github.com>
2023-04-20 17:29:04 +0700

    dev: finish task 3

--- solutions/task3.py
+++ solutions/task3.py
@@ -2,9 +2,12 @@
 Task 3: Scheduling CSR for 1 week, maximize workload fairness
 """
 import itertools
+import math
 from pathlib import Path
 
 import numpy as np
+import pandas as pd
+from scipy.optimize import linprog
 
 from task3_domain import CsrRequirePerDayDetails, ShiftSpanDetail, RawSchedule, ProblemInput
 
@@ -21,10 +24,9 @@ def main():
 
     solve_lp_problem(
         ProblemInput(
-            num_of_csr_i=raw_schedules.num_of_csr,
-            num_of_day_per_week_j=csr_requirement_per_day.num_of_day_per_week,
-            num_of_shifts_k=shifts_detail.num_of_available_shifts,
-            num_of_period_per_day_t=csr_requirement_per_day.num_of_period_per_day_t,
+            shift_span_detail=shifts_detail,
+            csr_requirement_detail=csr_requirement_per_day,
+            raw_schedules=raw_schedules,
         )
     )
 
@@ -39,13 +41,23 @@ def solve_lp_problem(problem_input: ProblemInput):
 
     coefficients_c = np.ones(problem_input.num_of_optimized_vars)
 
-    a_ub, b_ub = get_matrix_for_condition_one_shift_per_day(problem_input)
-    a_ub, b_ub = get_matrix_for_condition_one_day_off_per_week(problem_input)
+    a_ub1, b_ub1 = get_matrix_for_condition_one_shift_per_day(problem_input)
+    a_ub2, b_ub2 = get_matrix_for_condition_one_day_off_per_week(problem_input)
+    a_ub3, b_ub3 = get_matrix_for_condition_csr_num_required_per_period(problem_input)
+    a_ub4, b_ub4 = get_matrix_for_condition_csr_must_be_scheduled_fairly(problem_input)
+
+    total_a_ub = np.concatenate((a_ub1, a_ub2, a_ub3, a_ub4))
+    total_b_ub = np.concatenate((b_ub1, b_ub2, b_ub3, b_ub4))
+
+    result = linprog(coefficients_c, total_a_ub, total_b_ub, integrality=1)
+
+    dataframe_result = convert_to_pandas(result.x, problem_input)
+    print(dataframe_result)
 
 
 def get_matrix_for_condition_one_shift_per_day(
     problem_input: ProblemInput,
-) -> [np.ndarray, np.ndarray]:
+) -> tuple[np.ndarray, np.ndarray]:
     """
     :param problem_input:
 
@@ -77,7 +89,7 @@ def get_matrix_for_condition_one_shift_per_day(
 
 def get_matrix_for_condition_one_day_off_per_week(
     problem_input: ProblemInput,
-) -> [np.ndarray, np.ndarray]:
+) -> tuple[np.ndarray, np.ndarray]:
     """
     :param problem_input:
 
@@ -106,7 +118,7 @@ def get_matrix_for_condition_one_day_off_per_week(
 
 def get_matrix_for_condition_csr_num_required_per_period(
     problem_input: ProblemInput,
-) -> [np.ndarray, np.ndarray]:
+) -> tuple[np.ndarray, np.ndarray]:
     """
     Because we can only do ≤ inequality, we will perform inverse (-1)
     :param problem_input:
@@ -116,27 +128,93 @@ def get_matrix_for_condition_csr_num_required_per_period(
 
     in_eq_constraint_matrix = np.zeros(
         (
-            problem_input.num_of_csr_i,
+            problem_input.num_of_day_per_week_j * problem_input.num_of_period_per_day_t,
             problem_input.num_of_optimized_vars,
         )
     )
 
     for processing_row, (day_j, period_t) in enumerate(
         itertools.product(
-            range(problem_input.num_of_day_per_week_j), range(problem_input.num_of_period_per_day_t)
+            range(problem_input.num_of_day_per_week_j),
+            range(problem_input.num_of_period_per_day_t),
         )
     ):
         for csr_i, shift_k in itertools.product(
-            range(problem_input.num_of_day_per_week_j), range(problem_input.num_of_shifts_k)
+            range(problem_input.num_of_csr_i), range(problem_input.num_of_shifts_k)
         ):
-            optimized_var_loc = convert_i_j_k_to_1d_arr_loc(csr_i, day_j, shift_k, problem_input)
-            in_eq_constraint_matrix[processing_row, optimized_var_loc] = 1
+            period_is_spanned = problem_input.shift_span_detail.check_for_spanning(
+                shift_k, period_t
+            )
+            if not period_is_spanned:
+                continue
 
-    in_eq_constraint_vector = np.repeat(
-        problem_input.num_of_day_per_week_j - 1, problem_input.num_of_csr_i
+            optimized_var_loc = convert_i_j_k_to_1d_arr_loc(csr_i, day_j, shift_k, problem_input)
+            in_eq_constraint_matrix[processing_row, optimized_var_loc] = -1
+
+    in_eq_constraint_vector = -(
+        problem_input.csr_requirement_detail.get_vector_of_day_and_require_csr()
     )
 
     return in_eq_constraint_matrix, in_eq_constraint_vector
+
+
+def get_matrix_for_condition_csr_must_be_scheduled_fairly(
+    problem_input: ProblemInput,
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Because we can only do ≤ inequality, we will perform inverse (-1)
+    :param problem_input:
+
+    :return: A_ub & b_ub
+    """
+
+    num_of_total_required_csr_nc = problem_input.num_of_csr_i
+
+    in_eq_constraint_matrix_upper_bound = np.zeros(
+        (
+            problem_input.num_of_shifts_k * problem_input.num_of_csr_i,
+            problem_input.num_of_optimized_vars,
+        )
+    )
+
+    in_eq_constraint_vector = []
+
+    for processing_row, (shift_k, csr_i) in enumerate(
+        itertools.product(range(problem_input.num_of_shifts_k), range(problem_input.num_of_csr_i))
+    ):
+        num_of_csr_for_shift_nc_k = problem_input.required_csr_per_shift_nc_k[shift_k]
+        upper_bound = math.ceil(num_of_csr_for_shift_nc_k / num_of_total_required_csr_nc)
+
+        for day_j in range(problem_input.num_of_day_per_week_j):
+            optimized_var_loc = convert_i_j_k_to_1d_arr_loc(csr_i, day_j, shift_k, problem_input)
+            in_eq_constraint_matrix_upper_bound[processing_row, optimized_var_loc] = 1
+
+        in_eq_constraint_vector.append(upper_bound)
+
+    in_eq_constraint_matrix_lower_bound = np.zeros(
+        (
+            problem_input.num_of_shifts_k * problem_input.num_of_csr_i,
+            problem_input.num_of_optimized_vars,
+        )
+    )
+
+    for processing_row, (shift_k, csr_i) in enumerate(
+        itertools.product(range(problem_input.num_of_shifts_k), range(problem_input.num_of_csr_i))
+    ):
+        num_of_csr_for_shift_nc_k = problem_input.required_csr_per_shift_nc_k[shift_k]
+        lower_bound = math.floor(num_of_csr_for_shift_nc_k / num_of_total_required_csr_nc)
+
+        for day_j in range(problem_input.num_of_day_per_week_j):
+            optimized_var_loc = convert_i_j_k_to_1d_arr_loc(csr_i, day_j, shift_k, problem_input)
+            in_eq_constraint_matrix_lower_bound[processing_row, optimized_var_loc] = -1
+
+        in_eq_constraint_vector.append(-lower_bound)
+
+    in_eq_constraint_matrix = np.vstack(
+        (in_eq_constraint_matrix_upper_bound, in_eq_constraint_matrix_lower_bound)
+    )
+
+    return in_eq_constraint_matrix, np.array(in_eq_constraint_vector)
 
 
 def convert_i_j_k_to_1d_arr_loc(
@@ -147,6 +225,32 @@ def convert_i_j_k_to_1d_arr_loc(
         + (day_j * problem_input.num_of_shifts_k)
         + shift_k
     )
+
+
+def convert_to_pandas(solution: np.array, problem_input: ProblemInput) -> pd.DataFrame:
+    reshaped: np.ndarray = solution.reshape(
+        (
+            problem_input.num_of_csr_i,
+            problem_input.num_of_day_per_week_j,
+            problem_input.num_of_shifts_k,
+        )
+    )
+
+    csr_names = problem_input.raw_schedules.get_csr_names()
+    shift_names = problem_input.shift_span_detail.get_shift_names()
+    day_of_week_names = problem_input.csr_requirement_detail.get_day_of_week_names()
+
+    parsed_result = pd.DataFrame(
+        "",
+        index=csr_names,
+        columns=day_of_week_names,
+    )
+
+    for index_combination in np.argwhere(reshaped != 0):
+        csr_i, day_j, shift_k = tuple(index_combination)
+        parsed_result.iloc[csr_i, day_j] = shift_names[shift_k]
+
+    return parsed_result
 
 
 if __name__ == "__main__":
--- solutions/task3_domain.py
+++ solutions/task3_domain.py
@@ -1,13 +1,18 @@
 import json
+from collections import defaultdict
 from dataclasses import field, dataclass
 from pathlib import Path
 from typing import Optional, List, Dict
+
+import numpy as np
 
 
 class CsrRequirePerDayDetails:
     def __init__(self, input_file: Path):
         with open(input_file, "r") as input_json:
-            self._input_details: Dict[str, List[int]] = json.load(input_json)
+            parsed_input = json.load(input_json)
+            self._day_of_week_names = list(parsed_input.keys())
+            self._csr_requirements_per_day = np.array(list(parsed_input.values()))
 
     @staticmethod
     def from_json_file(file_name: Path):
@@ -15,18 +20,29 @@ class CsrRequirePerDayDetails:
 
     @property
     def num_of_day_per_week(self) -> int:
-        return len(self._input_details)
+        return self._csr_requirements_per_day.shape[0]
 
     @property
     def num_of_period_per_day(self) -> int:
-        first_any_day_detail = next(iter(self._input_details.values()))
-        return len(first_any_day_detail)
+        return self._csr_requirements_per_day.shape[1]
+
+    def get_vector_of_day_and_require_csr(self) -> np.ndarray:
+        """
+        :return: a 1D vector,  No.CSR major
+        """
+        return self._csr_requirements_per_day.flatten()
+
+    def get_day_of_week_names(self) -> List[str]:
+        return self._day_of_week_names
 
 
 class ShiftSpanDetail:
     def __init__(self, input_file: Path):
-        with open(input_file, "r") as input_json:
-            self._input_span_detail: Dict[str, List[int]] = json.load(input_json)
+        with open(input_file, "r") as input_file:
+            parsed_input: Dict[str, List[int]] = json.load(input_file)
+
+            self._shift_names = parsed_input.keys()
+            self._period_detail_matrix = np.array(list(parsed_input.values()))
 
     @staticmethod
     def from_json_file(file_name: Path):
@@ -34,19 +50,19 @@ class ShiftSpanDetail:
 
     @property
     def num_of_available_shifts(self) -> int:
-        return len(self._input_span_detail)
+        return self._period_detail_matrix.shape[0]
+
+    def get_shift_names(self) -> List[str]:
+        return list(self._shift_names)
+
+    def check_for_spanning(self, shift_k: int, period_t: int) -> bool:
+        return self._period_detail_matrix[shift_k, period_t]
 
 
 class RawSchedule:
     def __init__(self, input_file: Path):
         with open(input_file, "r") as input_json:
             self._input_schedule: Dict[str, List[Optional[str]]] = json.load(input_json)
-
-        self._available_shifts = set[str]()
-        for employee_schedule in self._input_schedule.values():
-            for shifts in employee_schedule:
-                if shifts:
-                    self._available_shifts.add(shifts)
 
     @staticmethod
     def from_json_file(file_name: Path):
@@ -55,21 +71,56 @@ class RawSchedule:
     def __repr__(self):
         return str(self._input_schedule)
 
+    def get_csr_names(self) -> List[str]:
+        return list(self._input_schedule.keys())
+
     @property
     def num_of_csr(self) -> int:
         return len(self._input_schedule)
 
+    def get_list_of_required_csr_per_shift_nc_k(self, shift_names: List[str]) -> List[int]:
+        """
+        :param shift_names: The order of appearing in input
+                is also the order of appearing for output
+        :return:
+        """
+        _csr_counter_per_shift = defaultdict(int)
+
+        for employee_schedule in self._input_schedule.values():
+            for shift in employee_schedule:
+                if shift:
+                    _csr_counter_per_shift[shift] += 1
+
+        return [_csr_counter_per_shift[shift] for shift in shift_names]
+
 
 @dataclass
 class ProblemInput:
-    num_of_csr_i: int
-    num_of_day_per_week_j: int
-    num_of_shifts_k: int
-    num_of_period_per_day_t: int
+    num_of_csr_i: int = field(init=False)
+    num_of_day_per_week_j: int = field(init=False)
+    num_of_shifts_k: int = field(init=False)
+    num_of_period_per_day_t: int = field(init=False)
+
+    shift_span_detail: ShiftSpanDetail
+    csr_requirement_detail: CsrRequirePerDayDetails
+    raw_schedules: RawSchedule
+
+    required_csr_per_shift_nc_k: List[int] = field(init=False)
 
     num_of_optimized_vars: int = field(init=False)
 
     def __post_init__(self):
+        self.num_of_csr_i = self.raw_schedules.num_of_csr
+        self.num_of_day_per_week_j = self.csr_requirement_detail.num_of_day_per_week
+        self.num_of_shifts_k = self.shift_span_detail.num_of_available_shifts
+        self.num_of_period_per_day_t = self.csr_requirement_detail.num_of_period_per_day
+
+        self.required_csr_per_shift_nc_k = (
+            self.raw_schedules.get_list_of_required_csr_per_shift_nc_k(
+                self.shift_span_detail.get_shift_names()
+            )
+        )
+
         self.num_of_optimized_vars = (
             self.num_of_csr_i * self.num_of_day_per_week_j * self.num_of_shifts_k
         )
